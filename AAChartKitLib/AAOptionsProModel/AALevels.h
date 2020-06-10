be73cbb28d602dc724e774d7ad958fa95b135235
An An <2236368544@qq.com>
2020-06-10 21:50:40 +0800

    Add all properties for AALevels

--- AAChartKitLib/AAOptionsProModel/AALevels.h
+++ AAChartKitLib/AAOptionsProModel/AALevels.h
@@ -9,8 +9,27 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@class AADataLabels, AAColorVariation;
 
 @interface AALevels : NSObject
+
+AAPropStatementAndPropSetFuncStatement(copy,   AALevels, NSString *, borderColor)
+AAPropStatementAndPropSetFuncStatement(copy,   AALevels, NSString *, borderDashStyle)
+AAPropStatementAndPropSetFuncStatement(strong, AALevels, NSNumber *, borderWidth)
+AAPropStatementAndPropSetFuncStatement(copy,   AALevels, NSString *, color)
+AAPropStatementAndPropSetFuncStatement(assign, AALevels, id        , colorByPoint) 
+AAPropStatementAndPropSetFuncStatement(strong, AALevels, AADataLabels *, dataLabels)
+AAPropStatementAndPropSetFuncStatement(copy,   AALevels, NSString *, layoutAlgorithm)
+AAPropStatementAndPropSetFuncStatement(copy,   AALevels, NSString *, layoutStartingDirection)
+AAPropStatementAndPropSetFuncStatement(strong, AALevels, NSNumber *, level)
+AAPropStatementAndPropSetFuncStatement(strong, AALevels, AAColorVariation *, colorVariation)
+
+@end
+
+@interface AAColorVariation : NSObject
+
+AAPropStatementAndPropSetFuncStatement(copy,   AAColorVariation, NSString *, key)
+AAPropStatementAndPropSetFuncStatement(strong, AAColorVariation, NSNumber *, to)
 
 @end
 
--- AAChartKitLib/AAOptionsProModel/AALevels.m
+++ AAChartKitLib/AAOptionsProModel/AALevels.m
@@ -10,4 +10,22 @@
 
 @implementation AALevels
 
+AAPropSetFuncImplementation(AALevels, NSString *, borderColor)
+AAPropSetFuncImplementation(AALevels, NSString *, borderDashStyle)
+AAPropSetFuncImplementation(AALevels, NSNumber *, borderWidth)
+AAPropSetFuncImplementation(AALevels, NSString *, color)
+AAPropSetFuncImplementation(AALevels, id        , colorByPoint)
+AAPropSetFuncImplementation(AALevels, AADataLabels *, dataLabels)
+AAPropSetFuncImplementation(AALevels, NSString *, layoutAlgorithm)
+AAPropSetFuncImplementation(AALevels, NSString *, layoutStartingDirection)
+AAPropSetFuncImplementation(AALevels, NSNumber *, level)
+AAPropSetFuncImplementation(AALevels, AAColorVariation *, colorVariation)
+
+@end
+
+@implementation AAColorVariation
+
+AAPropSetFuncImplementation(AAColorVariation, NSString *, key)
+AAPropSetFuncImplementation(AAColorVariation, NSNumber *, to)
+
 @end
