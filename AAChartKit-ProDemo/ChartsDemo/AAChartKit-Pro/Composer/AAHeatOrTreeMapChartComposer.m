ea3b8c437d10a9d5c73f6491b71e4202a06a99a2
An An <2236368544@qq.com>
2023-05-31 15:14:51 +0800

    Update AAHeatOrTreeMapChartComposer.m

--- AAChartKit-ProDemo/ChartsDemo/AAChartKit-Pro/Composer/AAHeatOrTreeMapChartComposer.m
+++ AAChartKit-ProDemo/ChartsDemo/AAChartKit-Pro/Composer/AAHeatOrTreeMapChartComposer.m
@@ -608,6 +608,208 @@
     return aaOptions;
 }
 
+//configure tree graph chart options
+//{
+//    chart: {
+//        inverted: true,
+//        marginBottom: 170
+//    },
+//    title: {
+//        text: 'Inverted treegraph',
+//        align: 'left'
+//    },
+//    series: [
+//        {
+//            type: 'treegraph',
+//            data,
+//            tooltip: {
+//                pointFormat: '{point.name}'
+//            },
+//            dataLabels: {
+//                pointFormat: '{point.name}',
+//                style: {
+//                    whiteSpace: 'nowrap',
+//                    color: '#000000',
+//                    textOutline: '3px contrast'
+//                },
+//                crop: false
+//            },
+//            marker: {
+//                radius: 6
+//            },
+//            levels: [
+//                {
+//                    level: 1,
+//                    dataLabels: {
+//                        align: 'left',
+//                        x: 20
+//                    }
+//                },
+//                {
+//                    level: 2,
+//                    colorByPoint: true,
+//                    dataLabels: {
+//                        verticalAlign: 'bottom',
+//                        y: -20
+//                    }
+//                },
+//                {
+//                    level: 3,
+//                    colorVariation: {
+//                        key: 'brightness',
+//                        to: -0.5
+//                    },
+//                    dataLabels: {
+//                        align: 'left',
+//                        rotation: 90,
+//                        y: 20
+//                    }
+//                }
+//            ]
+//        }
+//    ]
+//}
+
++ (AAOptions *)invertedTreegraph {
+    return AAOptions.new
+    .chartSet(AAChart.new
+              .invertedSet(true)
+              .marginBottomSet(@170))
+    .titleSet(AATitle.new
+              .textSet(@"Inverted treegraph")
+              .alignSet(AAChartAlignTypeLeft))
+    .seriesSet(@[
+        AASeriesElement.new
+//        .typeSet(AAChartTypeTreeMap)
+//        .dataSet(AAOptionsData.treeData)
+        .tooltipSet(AATooltip.new
+                    .pointFormatSet(@"{point.name}"))
+        .dataLabelsSet(AADataLabels.new
+//                       .pointFormatSet(@"{point.name}")
+                       .styleSet(AAStyle.new
+//                                 .whiteSpaceSet(AAChartWhiteSpaceTypeNowrap)
+                                 .colorSet(@"#000000")
+                                 .textOutlineSet(@"3px contrast"))
+                       .cropSet(false))
+        .markerSet(AAMarker.new
+                   .radiusSet(@6))
+        .levelsSet(@[
+            AALevelsElement.new
+            .levelSet(@1)
+            .dataLabelsSet(AADataLabels.new
+                           .alignSet(AAChartAlignTypeLeft)
+                           .xSet(@20)),
+            AALevelsElement.new
+            .levelSet(@2)
+            .colorByPointSet(@true)
+            .dataLabelsSet(AADataLabels.new
+                           .verticalAlignSet(AAChartVerticalAlignTypeBottom)
+                           .ySet(@(-20))),
+            AALevelsElement.new
+            .levelSet(@3)
+            .colorVariationSet(AAColorVariation.new
+                               .keySet(@"brightness")
+                               .toSet(@(-0.5)))
+            .dataLabelsSet(AADataLabels.new
+                           .alignSet(AAChartAlignTypeLeft)
+                           .rotationSet(@90)
+                           .ySet(@20))
+        ])
+    ]);
+}
+
+//{
+//    title: {
+//        text: 'Treegraph with box layout'
+//    },
+//    series: [
+//        {
+//            type: 'treegraph',
+//            data,
+//            tooltip: {
+//                pointFormat: '{point.name}'
+//            },
+//            marker: {
+//                symbol: 'rect',
+//                width: '25%'
+//            },
+//            borderRadius: 10,
+//            dataLabels: {
+//                pointFormat: '{point.name}',
+//                style: {
+//                    whiteSpace: 'nowrap'
+//                }
+//            },
+//            levels: [
+//                {
+//                    level: 1,
+//                    levelIsConstant: false
+//                },
+//                {
+//                    level: 2,
+//                    colorByPoint: true
+//                },
+//                {
+//                    level: 3,
+//                    colorVariation: {
+//                        key: 'brightness',
+//                        to: -0.5
+//                    }
+//                },
+//                {
+//                    level: 4,
+//                    colorVariation: {
+//                        key: 'brightness',
+//                        to: 0.5
+//                    }
+//                }
+//            ]
+//        }
+//    ]
+//}
+
++ (AAOptions *)treegraphWithBoxLayout {
+    return AAOptions.new
+    .titleSet(AATitle.new
+              .textSet(@"Treegraph with box layout"))
+    .seriesSet(@[
+        AASeriesElement.new
+//        .typeSet(AAChartTypeTreeMap)
+//        .dataSet(AAOptionsData.treeData)
+        .tooltipSet(AATooltip.new
+                    .pointFormatSet(@"{point.name}"))
+        .markerSet(AAMarker.new
+                   .symbolSet(@"rect")
+                   .widthSet(@"25%"))
+        .borderRadiusSet(@10)
+        .dataLabelsSet(AADataLabels.new
+//                       .pointFormatSet(@"{point.name}")
+                       .styleSet(AAStyle.new
+//                                 .whiteSpaceSet(AAChartWhiteSpaceTypeNowrap)
+                       ))
+        .levelsSet(@[
+            AALevelsElement.new
+            .levelSet(@1)
+//            .levelIsConstantSet(false)
+            ,
+            AALevelsElement.new
+            .levelSet(@2)
+            .colorByPointSet(@true),
+            AALevelsElement.new
+            .levelSet(@3)
+            .colorVariationSet(AAColorVariation.new
+                               .keySet(@"brightness")
+                               .toSet(@(-0.5))),
+            AALevelsElement.new
+            .levelSet(@4)
+            .colorVariationSet(AAColorVariation.new
+                               .keySet(@"brightness")
+                               .toSet(@0.5))
+        ])
+    ]);
+}
+
+
 
 + (NSString *)aa_toPureJSString2WithString:(NSString *)string {
     //https://stackoverflow.com/questions/34334232/why-does-function-not-work-but-function-does-chrome-devtools-node
