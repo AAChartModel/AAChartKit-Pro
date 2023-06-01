81b7f1a5f4aa1651ec53a2a6598eea16581df2e5
An An <2236368544@qq.com>
2023-06-01 11:31:06 +0800

    Add AAPattern

---
+++ AAChartKit-Pro/AAOptionsProModel/AAPattern.h
@@ -0,0 +1,30 @@
+//
+//  AAPattern.h
+//  AAChartKit-ProDemo
+//
+//  Created by AnAn on 2023/6/1.
+//  Copyright © 2023 An An. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AAPattern : NSObject
+
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, aspectRatio)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSString *, backgroundColor)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSString *, color)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, height)
+AAPropStatementAndPropSetFuncStatement(copy,   AAPattern, NSString *, id)
+AAPropStatementAndPropSetFuncStatement(copy,   AAPattern, NSString *, image)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, opacity)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSString *, path)
+AAPropStatementAndPropSetFuncStatement(copy,   AAPattern, NSString *, patternTransform)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, width)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, x)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, y)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AAChartKit-Pro/AAOptionsProModel/AAPattern.m
@@ -0,0 +1,26 @@
+//
+//  AAPattern.m
+//  AAChartKit-ProDemo
+//
+//  Created by AnAn on 2023/6/1.
+//  Copyright © 2023 An An. All rights reserved.
+//
+
+#import "AAPattern.h"
+
+@implementation AAPattern
+
+AAPropSetFuncImplementation(AAPattern, NSNumber *, aspectRatio)
+AAPropSetFuncImplementation(AAPattern, NSString *, backgroundColor)
+AAPropSetFuncImplementation(AAPattern, NSString *, color)
+AAPropSetFuncImplementation(AAPattern, NSNumber *, height)
+AAPropSetFuncImplementation(AAPattern, NSString *, id)
+AAPropSetFuncImplementation(AAPattern, NSString *, image)
+AAPropSetFuncImplementation(AAPattern, NSNumber *, opacity)
+AAPropSetFuncImplementation(AAPattern, NSString *, path)
+AAPropSetFuncImplementation(AAPattern, NSString *, patternTransform)
+AAPropSetFuncImplementation(AAPattern, NSNumber *, width)
+AAPropSetFuncImplementation(AAPattern, NSNumber *, x)
+AAPropSetFuncImplementation(AAPattern, NSNumber *, y)
+
+@end
