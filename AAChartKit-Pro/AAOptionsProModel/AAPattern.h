bc8ef2f6b5c7c92a7e9a99008f2c5ce6d7148393
An An <2236368544@qq.com>
2023-06-01 14:39:20 +0800

    Update AAPattern.h

--- AAChartKit-Pro/AAOptionsProModel/AAPattern.h
+++ AAChartKit-Pro/AAOptionsProModel/AAPattern.h
@@ -4,26 +4,39 @@
 //
 //  Created by AnAn on 2023/6/1.
 //  Copyright © 2023 An An. All rights reserved.
-//  https://api.highcharts.com/class-reference/Highcharts.PatternOptionsObject#toc0
+//
 
 #import <Foundation/Foundation.h>
 
+@class AAPatternOptions;
+
 NS_ASSUME_NONNULL_BEGIN
 
+//https://api.highcharts.com/class-reference/Highcharts.PatternObject#toc0
 @interface AAPattern : NSObject
 
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, aspectRatio)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSString *, backgroundColor)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSString *, color)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, height)
-AAPropStatementAndPropSetFuncStatement(copy,   AAPattern, NSString *, id)
-AAPropStatementAndPropSetFuncStatement(copy,   AAPattern, NSString *, image)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, opacity)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSString *, path)
-AAPropStatementAndPropSetFuncStatement(copy,   AAPattern, NSString *, patternTransform)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, width)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, x)
-AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, y)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, id, animation)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, AAPatternOptions *, pattern)
+AAPropStatementAndPropSetFuncStatement(strong, AAPattern, NSNumber *, patternIndex)
+
+@end
+
+
+//https://api.highcharts.com/class-reference/Highcharts.PatternOptionsObject#toc0
+@interface AAPatternOptions : NSObject
+
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSNumber *, aspectRatio)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSString *, backgroundColor)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSString *, color)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSNumber *, height)
+AAPropStatementAndPropSetFuncStatement(copy,   AAPatternOptions, NSString *, id)
+AAPropStatementAndPropSetFuncStatement(copy,   AAPatternOptions, NSString *, image)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSNumber *, opacity)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSString *, path)
+AAPropStatementAndPropSetFuncStatement(copy,   AAPatternOptions, NSString *, patternTransform)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSNumber *, width)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSNumber *, x)
+AAPropStatementAndPropSetFuncStatement(strong, AAPatternOptions, NSNumber *, y)
 
 @end
 
