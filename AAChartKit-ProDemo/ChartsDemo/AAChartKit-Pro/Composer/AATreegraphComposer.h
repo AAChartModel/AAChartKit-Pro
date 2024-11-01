69e1dbf1ef0326bdc09f6c9888d0af63f018dc21
An An <2236368544@qq.com>
2024-11-01 16:06:55 +0800

    Add AATreegraphComposer

---
+++ AAChartKit-ProDemo/ChartsDemo/AAChartKit-Pro/Composer/AATreegraphComposer.h
@@ -0,0 +1,17 @@
+//
+//  AATreegraphComposer.h
+//  AAChartKit-ProDemo
+//
+//  Created by AnAn on 2024/11/1.
+//  Copyright © 2024 An An. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AATreegraphComposer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
