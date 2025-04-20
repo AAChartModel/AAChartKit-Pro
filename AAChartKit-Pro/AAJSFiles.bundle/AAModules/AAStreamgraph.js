c6c9602e5941f92d5a04ae9b0d77ceeb7ce822d7
An An <2236368544@qq.com>
2025-04-20 14:41:28 +0800

    Update to Highcharts `12.2.0` version

--- AAChartKit-Pro/AAJSFiles.bundle/AAModules/AAStreamgraph.js
+++ AAChartKit-Pro/AAJSFiles.bundle/AAModules/AAStreamgraph.js
@@ -1,13 +1,1 @@
-/*
- Highcharts JS v11.0.1 (2023-05-08)
-
- Streamgraph module
-
- (c) 2010-2021 Torstein Honsi
-
- License: www.highcharts.com/license
-*/
-'use strict';(function(a){"object"===typeof module&&module.exports?(a["default"]=a,module.exports=a):"function"===typeof define&&define.amd?define("highcharts/modules/streamgraph",["highcharts"],function(b){a(b);a.Highcharts=b;return a}):a("undefined"!==typeof Highcharts?Highcharts:void 0)})(function(a){function b(a,c,b,e){a.hasOwnProperty(c)||(a[c]=e.apply(null,b),"function"===typeof CustomEvent&&window.dispatchEvent(new CustomEvent("HighchartsModuleLoaded",{detail:{path:c,module:a[c]}})))}a=a?a._modules:
-{};b(a,"Series/Streamgraph/StreamgraphSeries.js",[a["Core/Series/SeriesRegistry.js"],a["Core/Utilities.js"]],function(a,b){const {seriesTypes:{areaspline:c}}=a,{merge:e,extend:f}=b;class d extends c{constructor(){super(...arguments);this.options=this.points=this.data=void 0}streamStacker(a,b,c){a[0]-=b.total/2;a[1]-=b.total/2;this.stackedYData[c]=a}}d.defaultOptions=e(c.defaultOptions,{fillOpacity:1,lineWidth:0,marker:{enabled:!1},stacking:"stream"});f(d.prototype,{negStacks:!1});a.registerSeriesType("streamgraph",
-d);"";return d});b(a,"masters/modules/streamgraph.src.js",[],function(){})});
-//# sourceMappingURL=streamgraph.js.map
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t(e._Highcharts,e._Highcharts.SeriesRegistry):"function"==typeof define&&define.amd?define("highcharts/modules/streamgraph",["highcharts/highcharts"],function(e){return t(e,e.SeriesRegistry)}):"object"==typeof exports?exports["highcharts/modules/streamgraph"]=t(e._Highcharts,e._Highcharts.SeriesRegistry):e.Highcharts=t(e.Highcharts,e.Highcharts.SeriesRegistry)}("undefined"==typeof window?this:window,(e,t)=>(()=>{"use strict";var r={512:e=>{e.exports=t},944:t=>{t.exports=e}},a={};function s(e){var t=a[e];if(void 0!==t)return t.exports;var i=a[e]={exports:{}};return r[e](i,i.exports,s),i.exports}s.n=e=>{var t=e&&e.__esModule?()=>e.default:()=>e;return s.d(t,{a:t}),t},s.d=(e,t)=>{for(var r in t)s.o(t,r)&&!s.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},s.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t);var i={};s.d(i,{default:()=>u});var o=s(944),n=s.n(o),h=s(512),d=s.n(h);let{areaspline:p}=d().seriesTypes,{addEvent:c,merge:l,extend:f}=n();class g extends p{streamStacker(e,t,r){e[0]-=t.total/2,e[1]-=t.total/2,this.stackedYData&&(this.stackedYData[r]=Math.max.apply(0,e))}}g.defaultOptions=l(p.defaultOptions,{fillOpacity:1,lineWidth:0,marker:{enabled:!1},stacking:"stream"}),c(g,"afterGetExtremes",e=>{e.dataExtremes.dataMin=-e.dataExtremes.dataMax}),f(g.prototype,{negStacks:!1}),d().registerSeriesType("streamgraph",g);let u=n();return i.default})());
